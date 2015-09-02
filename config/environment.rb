5a262ab1265351d7e38dd1453a9d73cef2a9873f
Melanie German <melger@me.com>
2015-09-02 23:13:54 +0000

    Changed username and password back, removed EVN

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => 'YXBwMzg3MTEyOTNAaGVyb2t1LmNvbQ==',
-  :password => 'aWl5ejIxajk1MDQ5',
+  :user_name => 'app38711293@heroku.com',
+  :password => 'iiyz21j95049',
   :domain => 'evening-cove-2748.herokuapp.com',
   :enable_startstls_auto => true
 }
